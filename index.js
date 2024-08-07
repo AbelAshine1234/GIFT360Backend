e0c68ec5d176b8d2759023b8a9c89565e6d6de21
abelashine <abelashine@spin.fashion>
2024-08-07 23:42:23 +0300

    updated

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ async function waitForTranscriptionJobToComplete(token, jobid) {
 async function createFinalTranscription(videoName, videoPath) {
   const token = await getToken(CLIENT_ID, CLIENT_SECRET);
   const data = await generateUploadUrl(token);
-  // await uploadVideo(data.signedUrl, videoName, videoPath);
+  await uploadVideo(data.signedUrl, videoName, videoPath);
   // const jobid = await createTranscription(token, data.url, "en-US");
   // const transcriptiondata = await waitForTranscriptionJobToComplete(
   //   token,
